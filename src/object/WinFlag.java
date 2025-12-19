71b4840c78c351e60b15645212ac3974d97df931
dSam <dSamlam1127@gmail.com>
2025-12-19 23:24:43 +0700

    a

---
+++ src/object/WinFlag.java
@@ -0,0 +1,20 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class WinFlag extends SuperObject {
+
+    public WinFlag() {
+        name = "Flag";
+
+        try {
+            image1 = ImageIO.read(getClass().getResourceAsStream("/objects/flag.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        collision = true; // cần va chạm
+    }
+}
+
