3aa9d09f702c58d865f16c0f60a3685dac966425
dSam <dSamlam1127@gmail.com>
2025-12-20 00:11:00 +0700

    Xoa cmt

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -40,8 +40,9 @@ public class Player extends Entity {
         maxHealth = 6;
         health = maxHealth;
         attack = 1;
-        worldX = gp.tileSize * 7;
-        worldY = gp.tileSize * 9;
+        //Set Player Spawn
+        worldX = gp.tileSize * 2;
+        worldY = gp.tileSize * 4;
         speed = 4;
         directionX = "none";
         directionY = "none";
